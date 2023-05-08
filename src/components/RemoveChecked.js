c1b48a9256dbe15e4bc4464d59051a2b96389d64
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-05-08 10:38:43 -0400

    My to-do app

---
+++ src/components/RemoveChecked.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GrFormClose } from "react-icons/gr";
+
+const RemoveChecked = ({ clearTodos }) => {
+  return (
+    <button className='btn btn-outline-success mt-md-0 mt-2' onClick={clearTodos}>
+      Remove checked<GrFormClose size="25px" className='icon-fix'/>
+    </button>
+  )
+}
+
+export default RemoveChecked
