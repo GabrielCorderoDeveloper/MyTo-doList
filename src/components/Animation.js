f147daeb63cfd81dca3aa8174bb6e0249a28743f
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-05-20 12:22:15 -0400

    Fixed image location and import.

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Animation.css';
+import logo from '../assets/todo_logo.png';
 
 const Animation = () => {
   return (
     <div className="loading-container d-flex align-items-center justify-content-center">
         <div className="image-blur"> </div> {/* This will breate a light effect behind the logo */}
-        <img src="%PUBLIC_URL%/todo_logo.png" alt="Logo" />       
+        <img src={logo} alt="Logo" />       
     </div>
   )
 }
